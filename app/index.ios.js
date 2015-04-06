288f4e3815b85426da2c7ef8abf7b2fa404e8d70
Pavan Ravipati <psravipati@gmail.com>
2015-04-06 16:11:38 -0700

    (feat) Add text input field for chat messages

--- app/index.ios.js
+++ app/index.ios.js
@@ -5,24 +5,41 @@
 'use strict';
 
 var React = require('react-native');
+
+var MOCKED_CHAT_DATA = [
+  {member: 'Richard', message: 'To the apple store!'},
+]
 var {
   AppRegistry,
   StyleSheet,
   Text,
+  TextInput,
   View,
 } = React;
 
 var app = React.createClass({
   render: function() {
+    var chat = MOCKED_CHAT_DATA[0];
     return (
       <View style={styles.container}>
         <Text style={styles.instructions}>
-          This is where we will have all of our chats
+          {chat.member + ': ' + chat.message}
         </Text>
+        <Submit />
       </View>
     );
   }
 });
+
+var Submit = React.createClass({
+  render: function() {
+    return (
+    <View>
+      <TextInput style={{height: 20, width: 200, borderColor: 'gray', borderWidth: 1}} />
+    </View>
+    );
+  }
+})
 
 var styles = StyleSheet.create({
   container: {
