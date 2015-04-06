6460019314a65b7e3ec0a65021541023aeb6d441
Arian Faurtosh <arian@icloud.com>
2015-04-06 14:09:30 -0700

    added deploy scripts and deploy proccess

---
+++ deploy/beforeinstall.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# stop all node services
+forever stopall
+
+# remove all client/server files
+sudo rm -rf /home/ubuntu/public/client
+sudo rm -rf /home/ubuntu/public/server
+
+# make fresh directories
+mkdir /home/ubuntu/public/client
+mkdir /home/ubuntu/public/server
