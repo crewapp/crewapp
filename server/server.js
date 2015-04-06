6c8321483bddc09f882d1eda2a88513d485d705e
Arian Faurtosh <arian@icloud.com>
2015-04-06 14:12:58 -0700

    fix(bug): added a back directory

--- server/server.js
+++ server/server.js
@@ -2,6 +2,6 @@ var fs = require('fs');
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + '/client'));
+app.use(express.static(__dirname + '/../client'));
 console.log(__dirname);
 app.listen(process.env.PORT || 3000);
