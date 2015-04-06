f9fd625409ac6df32f9c6f8086ea957afeaacbf5
Richard Kho <rkho@me.com>
2015-04-06 12:09:24 -0700

    Started routing to database

--- server/database/config.js
+++ server/database/config.js
@@ -1,13 +1,22 @@
 var Sequelize = require('sequelize');
 
-var Messages = Sequelize.define('Messages', {
+var sequelize = new Sequelize(process.env['CLEAR_DB_DATABASE'], process.env['CLEAR_DB_USER'], process.env['CLEAR_DB_PW'], {
+  host: process.env['CLEAR_DB_SERVER'],
+  dialect: 'mysql'
+});)
+
+
+
+var Messages = sequelize.define('Messages', {
   message: Sequelize.TEXT,
   timestamp: Sequelize.DATE,
 });
 
-var Users = Sequelize.define('Users', {
+var Users = sequelize.define('Users', {
   name: Sequelize.STRING
 })
 
 Users.hasMany(Messages);
 Messages.hasOne(Users);
+
+module.exports = Database;
