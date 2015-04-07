0829f251eb40c385be9c91cd4c9eda729671c0b6
Richard Kho <rkho@me.com>
2015-04-06 18:57:51 -0700

    Removed timestamp from schema. Removed exports from routes.

--- server/routes.js
+++ server/routes.js
@@ -1,18 +1,16 @@
 var Database = require ('./database/config.js');
 
-module.exports = function(app){
-  // Add new user account to database
-  // app.post('/api/addnewuser', function(req, res){
-  //   Database.Users.create ({
-  //     name: req.body.username
-  //   })
-  // });
+// Add new user account to database
+// app.post('/api/addnewuser', function(req, res){
+//   Database.Users.create ({
+//     name: req.body.username
+//   })
+// });
 
-  // Add new message to database
-  app.post('/api/addnewmessage', function(req, res){
-    Database.Messages.create ({
-      message: req.body.message,
-      name: req.body.name
-    })
-  });
-}
+// Add new message to database
+app.post('/api/addnewmessage', function(req, res){
+  Database.Messages.create ({
+    message: req.body.message,
+    name: req.body.name
+  })
+});
