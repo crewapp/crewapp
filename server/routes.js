cec1c940a58653ecced1a01f4dca93babadaf23f
Richard Kho <rkho@me.com>
2015-04-06 18:59:39 -0700

    Returned exports in routes.js since it broke server

--- server/routes.js
+++ server/routes.js
@@ -1,16 +1,18 @@
 var Database = require ('./database/config.js');
 
-// Add new user account to database
-// app.post('/api/addnewuser', function(req, res){
-//   Database.Users.create ({
-//     name: req.body.username
-//   })
-// });
+module.exports = function(app){
+  // Add new user account to database
+  // app.post('/api/addnewuser', function(req, res){
+  //   Database.Users.create ({
+  //     name: req.body.username
+  //   })
+  // });
 
-// Add new message to database
-app.post('/api/addnewmessage', function(req, res){
-  Database.Messages.create ({
-    message: req.body.message,
-    name: req.body.name
-  })
-});
+  // Add new message to database
+  app.post('/api/addnewmessage', function(req, res){
+    Database.Messages.create ({
+      message: req.body.message,
+      name: req.body.name
+    })
+  });
+}
