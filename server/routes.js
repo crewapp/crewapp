1384bc2c05ebcd819f376c69cf04c72eb9d0b6d4
Richard Kho <rkho@me.com>
2015-04-06 19:59:13 -0700

    Changed routing to clean up the wrapper in module.exports. Added routing for get requests.

--- server/routes.js
+++ server/routes.js
@@ -1,18 +1,30 @@
 var Database = require ('./database/config.js');
 
-module.exports = function(app){
-  // Add new user account to database
-  // app.post('/api/addnewuser', function(req, res){
-  //   Database.Users.create ({
-  //     name: req.body.username
-  //   })
-  // });
+var router = require('express').Router();
 
-  // Add new message to database
-  app.post('/api/addnewmessage', function(req, res){
-    Database.Messages.create ({
-      message: req.body.message,
-      name: req.body.name
-    })
-  });
-}
+// Add new user account to database
+// app.post('/api/addnewuser', function(req, res){
+//   Database.Users.create ({
+//     name: req.body.username
+//   })
+// });
+
+// Add new message to database
+router.route('/api/message').post(function(req, res){
+  Database.Messages.create ({
+    message: req.body.message,
+    name: req.body.name
+  })
+});
+
+router.route('/api/message').get(function(req, res){
+  Database.Messages.findAll({
+    // returns an object for me to res.json it
+    limit: 50
+  }).success(function(data){
+    res.json(data);
+  })
+});
+
+
+module.exports = router;
