05f4b0ebd2ec3d983a05a46536834d724eea8d6c
Richard Kho <rkho@me.com>
2015-04-07 18:25:20 -0700

    Commented out database files, switching to other service for now.

--- server/database/config.js
+++ server/database/config.js
@@ -1,29 +1,29 @@
-var Sequelize = require('sequelize');
+// var Sequelize = require('sequelize');
 
-var dbconfig = {};
-dbconfig.database = process.env.database || 'crew';
-dbconfig.username = process.env.username || 'root';
-dbconfig.password = process.env.password || '';
-dbconfig.hostname = process.env.hostname || 'localhost';
+// var dbconfig = {};
+// dbconfig.database = process.env.database || 'crew';
+// dbconfig.username = process.env.username || 'root';
+// dbconfig.password = process.env.password || '';
+// dbconfig.hostname = process.env.hostname || 'localhost';
 
-var orm = new Sequelize(dbconfig.database, dbconfig.username, dbconfig.password, {
-  host: dbconfig.hostname
-});
+// var orm = new Sequelize(dbconfig.database, dbconfig.username, dbconfig.password, {
+//   host: dbconfig.hostname
+// });
 
-var Messages = orm.define('messages', {
-  message: Sequelize.TEXT,
-  name: Sequelize.STRING
-});
+// var Messages = orm.define('messages', {
+//   message: Sequelize.TEXT,
+//   name: Sequelize.STRING
+// });
 
-var Users = orm.define('users', {
-  name: Sequelize.STRING
-});
+// var Users = orm.define('users', {
+//   name: Sequelize.STRING
+// });
 
-Messages.sync();
-Users.sync();
+// Messages.sync();
+// Users.sync();
 
 // Users.hasMany(Messages);
 // Messages.hasOne(Users);
 
-exports.Messages = Messages;
-exports.Users = Users;
+// exports.Messages = Messages;
+// exports.Users = Users;
