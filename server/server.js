c5b5dcbc9b34ed571c5410d264ff37320175dca9
Arian Faurtosh <arian@icloud.com>
2015-04-09 13:19:35 -0700

    closes #57: Integrated the chat server with test dir

--- server/server.js
+++ server/server.js
@@ -1,8 +1,32 @@
-var fs = require('fs');
+//------ Require: General Server ----//
 var express = require('express');
+
+//------ Require: Chat Server -------//
+var chatApp = express();
+var http = require('http');
+var chatServer = http.Server(chatApp);
+var socket = require('socket.io');
+var io = socket(chatServer);
+
+//------- Require: Our Server -------//
 var app = express();
-var routes = require('./routes.js');
 var parser = require('body-parser');
+var routes = require('./routes.js');
+
+//------ Chat Server ------//
+
+chatApp.use(express.static(__dirname + '/../test'));
+
+io.on('connection', function(socket){
+  'use strict';
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
+chatServer.listen(process.env.CHATPORT || 5000);
+
+//------ Our App Server ------//
 
 app.use(express.static(__dirname + '/../client'));
 
