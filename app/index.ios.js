be312ce74a8c8a4fbf68654cb5a8cb5fc28962bb
Richard Kho <rkho@me.com>
2015-04-09 19:30:13 -0700

    Added 'Send' button to UI, hooked up to send function

--- app/index.ios.js
+++ app/index.ios.js
@@ -1,6 +1,8 @@
 'use strict';
 
+var styles = require('./styles.js');
 var React = require('react-native');
+var t = require('tcomb-form-native');
 var MOCKED_CHAT_DATA;
 
 var name;
@@ -13,29 +15,56 @@ var {
   View,
   ListView,
   NavigatorIOS,
-  AlertIOS
+  AlertIOS,
+  TouchableHighlight,
 } = React;
 
 var io = require('react-native-sockets-io');
 
+var Form = t.form.Form;
+
+var Message = t.struct({
+  name: t.Str,
+  message: t.Str
+});
+
+var options = {
+  fields: {
+    name: {
+      placeholder: 'Your name',
+      error: 'You have to enter a name!'
+    },
+    message: {
+      placeholder: 'Enter a message',
+      error: 'You have to enter a message!',
+      multiline: true
+    }
+  }
+};
+
+// App container (top of the tree)
 var app = React.createClass({
   getInitialState: function() {
     return {io: io('http://localhost:5000', {jsonp: false})};
   },
   componentDidMount: function() {
-    this.state.io.emit('chat message', "hello");
+    console.log('hooray');
   },
   render: function() {
     return (
       <View style={styles.container}>
-        <ChatList socket={this.state.io} />
-        <Name />
-        <Submit socket={this.state.io} />
+        <View style={styles.chatBox}>
+          <ChatList socket={this.state.io} />
+        </View>
+        <View style={styles.formBox}>
+          <MessageForm socket={this.state.io}  />
+        </View>
       </View>
     );
   }
 });
 
+// Navigation
 var Nav = React.createClass({
   render: function() {
     return (
@@ -50,6 +79,7 @@ var Nav = React.createClass({
   }
 });
 
+// List of messages
 var ChatList = React.createClass({
   getInitialState:function(){
     return {messages: []}
@@ -75,80 +105,84 @@ var ChatList = React.createClass({
   }
 });
 
+// Individual message
 var Chat = React.createClass({
   render: function() {
     return (
       <Text style={styles.text}>
-      {this.props.author}: {this.props.children}
+      {this.props.name}: {this.props.message}
       </Text>
     );
   }
 });
 
-var Name = React.createClass({
-  handleSubmit: function(e){
-    name = e.nativeEvent.text;
+// Username input field
+// var Name = React.createClass({
+//   handleSubmit: function(e){
+//     name = e.nativeEvent.text;
+//   },
+//   render: function() {
+//     return (
+//       <View>
+//         <TextInput
+//           placeholder="Please type in name"
+//           onSubmitEditing={(text) => this.handleSubmit(text)}
+//           style={styles.input} />
+//       </View>
+//     );
+//   }
+// });
+
+// Message input field
+// var Submit = React.createClass({
+//   handleSubmit: function(e) {
+//     var chat = e.nativeEvent.text;
+//     this.send(chat);
+//   },
+//   send: function(message) {
+//     this.props.socket.emit('chat message', message);
+//   },
+//   render: function() {
+//     return (
+//     <View>
+//       <TextInput
+//         onSubmitEditing={this.handleSubmit}
+//         style={styles.input} />
+//     </View>
+//     );
+//   }
+// });
+
+// Unified Input Field
+
+var MessageForm = React.createClass({
+  onPress: function() {
+    // call getValue to get values of the form
+
+    var value = this.refs.form.getValue();
+    if (value) {
+      this.send(value);
+    }
   },
+
+  send: function(message) {
+    this.props.socket.emit('chat message', message);
+  },
+
   render: function() {
     return (
-      <View>
-        <TextInput
-          placeholder="Please type in name"
-          onSubmitEditing={(text) => this.handleSubmit(text)}
-          style={styles.input} />
+      <View style={styles.container}>
+        {/* display */}
+        <Form
+          ref="form"
+          type={Message}
+          option={options}
+        />
+        <TouchableHighlight style={styles.button} onPress={this.onPress} underlayColor='#99d9f4'>
+          <Text style={styles.buttonText}>Send</Text>
+        </TouchableHighlight>
       </View>
     );
   }
 });
-
-var Submit = React.createClass({
-  handleSubmit: function(e) {
-    var chat = e.nativeEvent.text;
-    this.send(chat);
-  },
-  send: function(message) {
-    this.props.socket.emit('chat message', message);
-  },
-  render: function() {
-    this.send('hello');
-    return (
-    <View>
-      <TextInput
-        onSubmitEditing={this.handleSubmit}
-        style={styles.input} />
-    </View>
-    );
-  }
-});
-
-var styles = StyleSheet.create({
-  input: {
-    height: 36,
-    width:  200,
-    padding: 4,
-    marginRight: 5,
-    flex: 4,
-    fontSize: 18,
-    borderWidth: 1,
-    borderColor: '#48BBEC',
-    borderRadius: 8,
-    color: '#48BBEC'
-  },
-  nav: {
-    flex: 1
-  },
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'white',
-    marginTop: 20
-  },
-  text: {
-    color: 'black',
-    backgroundColor: 'white',
-    fontSize: 20
-  }
-});
-
 AppRegistry.registerComponent('app', () => app);
