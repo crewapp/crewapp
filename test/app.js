a0f1cca05e1e52ae583f4c191d63279b99c7eea0
koziscool <kozjob@yahoo.com>
2015-04-11 16:51:58 -0500

    closes #81: commit squash

--- test/app.js
+++ test/app.js
@@ -1,10 +1,12 @@
+'use strict';
+
 var socket = io();
 $('form').submit(function(){
-  var chat = $('#m').val()
+  var chat = $('#m').val();
   socket.emit('chat message', {name: 'anonymous', chat: chat});
   $('#m').val('');
   return false;
 });
 socket.on('chat message', function(msg){
-  $('#messages').append($('<li>').text(msg.name + ": " + msg.chat));
+  $('#messages').append($('<li>').text(msg.name + ': ' + msg.chat));
 });
