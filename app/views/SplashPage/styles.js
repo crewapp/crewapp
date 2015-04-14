90c110e7bee33821ba8d0fecbfe70e7bba35cf8a
Richard Kho <rkho@me.com>
2015-04-13 17:11:36 -0700

    Created a Splash Page which leads to Log In/Create Account views

---
+++ app/views/SplashPage/styles.js
@@ -0,0 +1,42 @@
+var React = require('react-native');
+var { StyleSheet } = React;
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#ffffff'
+  },
+  formBox: {
+    marginTop: 50,
+    padding: 20,
+    position: 'relative'
+  },
+  buttonText: {
+    fontSize: 18,
+    color: 'white',
+    alignSelf: 'center'
+  },
+  button: {
+    height: 36,
+    backgroundColor: '#48BBEC',
+    borderColor: '#48BBEC',
+    borderWidth: 1,
+    borderRadius: 8,
+    marginBottom: 10,
+    alignSelf: 'flex-start',
+    justifyContent: 'space-between'
+  },
+  grayButton: {
+    height: 36,
+    backgroundColor: '#CCCCCC',
+    borderColor: '#CCCCCC',
+    borderWidth: 1,
+    borderRadius: 8,
+    marginBottom: 10,
+    alignSelf: 'flex-end',
+    justifyContent: 'space-between'
+  }
+});
+
+module.exports = styles;
