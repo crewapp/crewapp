07314f6b357160109d4d33d58041777a46a459a5
Pavan Ravipati <psravipati@gmail.com>
2015-04-14 16:15:56 -0700

    closes #111: Add multi-room socket functionality to test chat client

--- test/app.js
+++ test/app.js
@@ -1,12 +1,32 @@
 'use strict';
 
-var socket = io();
-$('form').submit(function(){
-  var chat = $('#m').val();
-  socket.emit('chat message', {name: 'anonymous', chat: chat});
-  $('#m').val('');
-  return false;
-});
-socket.on('chat message', function(msg){
-  $('#messages').append($('<li>').text(msg.name + ': ' + msg.chat));
-});
+$(function() {
+
+  var socket = io();
+  var room;
+
+  $.ajax({
+    url:'http://localhost:5000/api/rooms',
+    success: function(data) {
+      console.log(data);
+      $('#header').html(data);
+      room = data;
+    }
+  }).then(function() {
+    socket.on('connect', function() {
+      socket.emit('join room', room);
+    });
+    socket.on('message', function(chat){
+      console.log('Message: ' + chat);
+      $('#messages').append('<li>anonymous: '+chat+'</li>');
+    });
+
+    $('form').submit(function(){
+      var chat = $('#m').val();
+      socket.emit('message', chat);
+      $('#m').val('');
+      return false;
+   });
+  });
+
+ });
