08faa659fe42e33efb7320c7bcbb627e56395468
Arian Faurtosh <arian@icloud.com>
2015-04-16 16:47:18 -0700

    closes #122: group api is now linked to db

--- server/groups/index.js
+++ server/groups/index.js
@@ -1,115 +1,186 @@
 'use strict';
 var router = require('express').Router();
+var db = require('./../database');
+var helper = require('./../helper');
+
 
 router.post('/', function(req, res){
   var username = req.body.username;
   var token = req.body.token;
-  var response;
 
   if(token === undefined || username === undefined){
-    response = {
+    res.json({
       response: 'failed',
       status: 'credentials not supplied'
-    };
-  }else if(token === '1010' && username === 'test'){
-    response = {
-      response: 'success',
-      group: 'crying panda'
-    };
-  }else{
-    response = {
-      response: 'failed',
-      status: 'invalid credentials'
-    };
+    });
+  }else {
+    db.User.find({
+      where: {
+        username: username,
+        token: token
+      }
+    }).then(function(user){
+      if(user !== null){
+        db.Group.find({
+          where: {
+            id: user.group_id
+          }
+        }).then(function(group){
+          var groupname = null;
+
+          if(group !== null){
+            groupname = group.groupname;
+          }
+          res.json({
+            response: 'success',
+            group: groupname
+          });
+        });
+      }else{
+        res.json({
+          response: 'failed',
+          status: 'invalid credentials'
+        });
+      }
+    });
   }
-  res.json(response);
 });
 
 router.post('/leave', function(req, res){
   var username = req.body.username;
   var token = req.body.token;
-  var response;
 
   if(token === undefined || username === undefined){
-    response = {
+    res.json({
       response: 'failed',
       status: 'credentials not supplied'
-    };
-  }else if(token === '1010' && username === 'test'){
-    response = {
-      response: 'success',
-      group: null
-    };
+    });
   }else{
-    response = {
-      response: 'failed',
-      status: 'invalid credentials'
-    };
+    db.User.find({
+      where: {
+        username: username,
+        token: token
+      }
+    }).then(function(user){
+      if(user === null){
+        res.json({
+          response: 'failed',
+          status: 'invalid credentials'
+        });
+      }else{
+        user.update({group_id: null}).then(function(){
+          res.json({
+            response: 'success',
+            group: null
+          });
+        });
+      }
+    });
   }
-  res.json(response);
 });
 
 router.post('/join', function(req, res){
   var username = req.body.username;
   var token = req.body.token;
-  var response;
 
   if(token === undefined || username === undefined){
-    response = {
+    res.json({
       response: 'failed',
       status: 'credentials not supplied'
-    };
-  }else if(token === '1010' && username === 'test'){
-    response = {
-      response: 'success',
-      group: 'dying turtle'
-    };
+    });
   }else{
-    response = {
-      response: 'failed',
-      status: 'invalid credentials'
-    };
+    db.User.find({
+      where: {
+        username: username,
+        token: token
+      }
+    }).then(function(user){
+      if(user === null){
+        res.json({
+          response: 'failed',
+          status: 'invalid credentials'
+        });
+      }else if(user.group_id !== null){
+        res.json({
+          response: 'failed',
+          status: 'already in group'
+        });
+      }else {
+        helper.findGroup(user, function(group){
+
+          db.GroupHistory.create({user_id: user.id, group_id: group.id})
+            .then(function(){
+
+              user.update({group_id: group.id}).then(function(){
+                res.json({
+                  response: 'success',
+                  token: user.token,
+                  group: group.groupname
+                });
+              });
+
+            });
+        });
+      }
+    });
   }
-  res.json(response);
 });
 
 router.post('/messages', function(req, res){
   var username = req.body.username;
   var token = req.body.token;
-  var response;
 
   if(token === undefined || username === undefined){
-    response = {
+    res.json({
       response: 'failed',
       status: 'credentials not supplied'
-    };
-  }else if(token === '1010' && username === 'test'){
-    // pull only info from user group
-    response = {
-      response: 'success',
-      group: 'dying turtle',
-      messages: [
-        {
-          'username': 'arian',
-          'message': 'hello guys'
-        },
-        {
-          'username': 'poppin3000',
-          'message': 'i love you guys'
-        },
-        {
-          'username': 'rkho',
-          'message': 'testing!!'
-        }
-      ]
-    };
+    });
   }else{
-    response = {
-      response: 'failed',
-      status: 'invalid credentials'
-    };
+    db.User.find({
+      where: {
+        username: username,
+        token: token
+      }
+    }).then(function(user){
+      if(user !== null){
+        db.Group.find({
+          where: {
+            id: user.group_id
+          }
+        }).then(function(group){
+          var groupname = null;
+
+          if(group !== null){
+            groupname = group.groupname;
+          }
+          res.json({
+            response: 'success',
+            group: groupname,
+            messages: [
+              {
+                'username': 'arian',
+                'message': 'hello guys'
+              },
+              {
+                'username': 'poppin3000',
+                'message': 'i love you guys'
+              },
+              {
+                'username': 'rkho',
+                'message': 'testing!!'
+              }
+            ]
+          });
+        });
+      }else{
+        res.json({
+          response: 'failed',
+          status: 'invalid credentials'
+        });
+      }
+    });
   }
-  res.json(response);
+
 });
 
 module.exports = router;
