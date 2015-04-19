360733c238c27fe5b7db44004fbbf5fe739a2539
Arian Faurtosh <arian@icloud.com>
2015-04-19 14:20:41 -0700

    (added): factors and auth

--- app2/www/js/signup/signup.js
+++ app2/www/js/signup/signup.js
@@ -1,8 +1,45 @@
 'use strict';
 angular.module('crewapp.signup', [])
-.controller('SignUpController', function($scope){
-  $scope.test = 'hello world';
-  $scope.submit = function(e) {
+.controller('SignUpController', function($scope, $window, $location, Auth){
+
+  $scope.form = {};
+
+  $scope.signup = function (e) {
+    var username = $scope.form.user;
+    var password = $scope.form.pass;
+
     e.stopPropagation();
+
+    Auth.signup({
+      username: username,
+      password: password
+
+    }).then(function (data) {
+
+      if(data.response === 'success'){
+
+        $window.localStorage.setItem('com.trycrewapp.username', username);
+        $window.localStorage.setItem('com.trycrewapp.token', data.token);
+        $window.localStorage.setItem('com.trycrewapp.group', data.group);
+
+        $location.path('/question');
+      }else if(data.response === 'failed'){
+        switch(data.status) {
+            case 'credentials not supplied':
+                window.alert('Please provide a username and password');
+                break;
+            case 'user exists':
+                window.alert('Username is taken!');
+                break;
+            default:
+                window.alert('error occured!');
+        }
+      }
+
+    }).catch(function (error) {
+
+      console.error(error);
+
+    });
   };
 });
