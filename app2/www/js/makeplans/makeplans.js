8539f5557d99d1267262e3c2c8fd5e590769d7ae
Richard Kho <rkho@me.com>
2015-04-21 21:51:06 -0700

    Grab Google Places image or placeholder if unavailable

--- app2/www/js/makeplans/makeplans.js
+++ app2/www/js/makeplans/makeplans.js
@@ -2,21 +2,27 @@
 angular.module('crewapp.makeplans', ['google.places'])
 .controller('MakePlansController', function($scope){
 
+  $scope.oneClear = true;
 
   $scope.makePlans = function(place) {
+    var photo = function() {
+      if (place.photos){
+        return place.photos[0].getUrl({'maxWidth': 150, 'maxHeight': 150});
+      } else {
+        return 'http://www.mtwomeybutchers.ie/wp-content/uploads/placeholder.gif';
+      }
+    }
     if (!$scope.firstPlace) {
-      $scope.firstPhoto = place.photos[0].getUrl({'maxWidth': 75, 'maxHeight': 75});
-      console.log($scope.firstPhoto)
+      $scope.firstPhoto = photo();
       $scope.firstPlace = place;
-      console.log(place);
     }
     else if (!$scope.secondPlace) {
-      $scope.secondPhoto = place.photos[0].getUrl({'maxWidth': 75, 'maxHeight': 75});
+      $scope.secondPhoto = photo();
       $scope.secondPlace = place;
+      $scope.oneClear = false;
     }
     else if ($scope.firstPlace && $scope.secondPlace) {
       console.log('finished!')
     }
   }
-
 })
