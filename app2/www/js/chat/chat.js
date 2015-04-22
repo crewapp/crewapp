c389c4c693ef29a4dc12466d40689fa88cdeddee
Pavan Ravipati <psravipati@gmail.com>
2015-04-21 23:18:12 -0700

    (feat) Add socket client library to app and connect to socket server

--- app2/www/js/chat/chat.js
+++ app2/www/js/chat/chat.js
@@ -1,6 +1,7 @@
 'use strict';
 angular.module('crewapp.chat', [])
-.controller('ChatController', function($scope){
+.controller('ChatController', function($scope, Auth, Sockets){
   $scope.test = 'Chats';
+  Sockets.rooms();
 
 });
