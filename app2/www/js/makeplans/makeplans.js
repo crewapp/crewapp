5454b7d9460f0198fec1a98cc627e7da83301b3a
Richard Kho <rkho@me.com>
2015-04-22 11:34:46 -0700

    Stores place information into an object and cleaned up layout

--- app2/www/js/makeplans/makeplans.js
+++ app2/www/js/makeplans/makeplans.js
@@ -2,27 +2,35 @@
 angular.module('crewapp.makeplans', ['google.places'])
 .controller('MakePlansController', function($scope){
 
-  $scope.oneClear = true;
+  $scope.bothEntered = true;
 
   $scope.makePlans = function(place) {
     var photo = function() {
-      if (place.photos){
+      if (!!place.photos){
         return place.photos[0].getUrl({'maxWidth': 150, 'maxHeight': 150});
       } else {
         return 'http://www.mtwomeybutchers.ie/wp-content/uploads/placeholder.gif';
       }
     }
-    if (!$scope.firstPlace) {
-      $scope.firstPhoto = photo();
-      $scope.firstPlace = place;
+    if (!$scope.one) {
+      $scope.one = {};
+      $scope.one.photo = photo();
+      $scope.one.name = place.name;
+      $scope.one.address = place.formatted_address
+      $scope.one.numVotes = 0;
+      console.log(place);
     }
     else if (!$scope.secondPlace) {
-      $scope.secondPhoto = photo();
-      $scope.secondPlace = place;
-      $scope.oneClear = false;
+      $scope.two = {};
+      $scope.two.photo = photo();
+      $scope.two.name = place.name;
+      $scope.two.address = place.formatted_address
+      $scope.two.numVotes = 0;
+      $scope.bothEntered = false;
     }
-    else if ($scope.firstPlace && $scope.secondPlace) {
-      console.log('finished!')
-    }
+  }
+
+  $scope.submitPlans = function() {
+    //send $scope.one and $scope.two to socketIO
   }
 })
