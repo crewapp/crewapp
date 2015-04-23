53ef6b1b1b13a0555c422f9d2f1a68ef1487cc5f
Pavan Ravipati <psravipati@gmail.com>
2015-04-23 15:25:46 -0700

    (Fix) Correct groupname and debugging sockets on ios

--- app2/www/js/chat/chat.js
+++ app2/www/js/chat/chat.js
@@ -3,6 +3,6 @@ angular.module('crewapp.chat', [])
 .controller('ChatController', function($scope, Auth, Sockets, $localStorage){
   $scope.test = 'Chats';
   Sockets.rooms();
-  Sockets.mySocket.emit('join room', $localStorage.groupname);
+  Sockets.mySocket.emit('join room', $localStorage.groupname)
 
 });
