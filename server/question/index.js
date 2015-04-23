2934373fbe1f8b809933589ab4cc00921c490a6e
Arian Faurtosh <arian@icloud.com>
2015-04-22 19:17:02 -0700

    fixes travis.ci issue

--- server/question/index.js
+++ server/question/index.js
@@ -1,11 +1,11 @@
 'use strict';
 var router = require('express').Router();
-var db = require('./../database');
-var helper = require('./../helper');
+// var db = require('./../database');
+// var helper = require('./../helper');
 
-router.post('/random', function(req, res){
+// router.post('/random', function(req, res){
 
 
-});
+// });
 
 module.exports = router;
