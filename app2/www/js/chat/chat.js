76920292424ea79167f435558bce4aec2916e2b4
Arian Faurtosh <arian@icloud.com>
2015-04-24 11:06:51 -0700

    fixed bug in socket.io

--- app2/www/js/chat/chat.js
+++ app2/www/js/chat/chat.js
@@ -1,8 +1,9 @@
 'use strict';
 angular.module('crewapp.chat', [])
-.controller('ChatController', function($scope, Auth, Sockets, $localStorage){
-  $scope.test = 'Chats';
-  Sockets.rooms();
-  Sockets.mySocket.emit('join room', $localStorage.groupname)
+.controller('ChatController', function($scope, Auth, Sockets){
+
+  ionic.Platform.ready(function(){
+    Sockets.emit('join room', 'pavan');
+  });
 
 });
