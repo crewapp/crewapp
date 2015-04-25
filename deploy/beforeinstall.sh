68c48067bb087d40bf229ddc949dc44141c6e212
Arian Faurtosh <arian@icloud.com>
2015-04-24 20:28:23 -0700

    closes #197: removes client from our repo

--- deploy/beforeinstall.sh
+++ deploy/beforeinstall.sh
@@ -4,12 +4,10 @@
 forever stopall
 
 # remove all client/server files
-sudo rm -rf /home/ubuntu/public/client
 sudo rm -rf /home/ubuntu/public/server
 sudo rm -rf /home/ubuntu/public/test
 
 # make fresh directories
-mkdir /home/ubuntu/public/client
 mkdir /home/ubuntu/public/server
 mkdir /home/ubuntu/public/test
 
