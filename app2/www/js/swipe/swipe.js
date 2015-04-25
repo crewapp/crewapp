0c9fde984ac00cda5520a5893dbec49ac041e32d
Richard Kho <rkho@me.com>
2015-04-24 21:37:51 -0700

    Added swiping

--- app2/www/js/swipe/swipe.js
+++ app2/www/js/swipe/swipe.js
@@ -1,6 +1,33 @@
 'use strict';
 angular.module('crewapp.swipe', [])
-.controller('SwipeController', function($scope){
+.controller('SwipeController', function($scope, TDCardDelegate){
+
   $scope.test = 'hello';
 
+  var cardTypes = [
+      { image: 'https://pbs.twimg.com/profile_images/546942133496995840/k7JAxvgq.jpeg' },
+      { image: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png' },
+      { image: 'https://pbs.twimg.com/profile_images/491995398135767040/ie2Z_V6e.jpeg' },
+    ];
+  $scope.cards = Array.prototype.slice.call(cardTypes, 0);
+
+  $scope.cardDestroyed = function(index) {
+    $scope.cards.splice(index, 1);
+  };
+
+  $scope.addCard = function() {
+    var newCard = cardTypes[Math.floor(Math.random() * cardTypes.length)];
+    newCard.id = Math.random();
+    $scope.cards.push(angular.extend({}, newCard));
+  }
+})
+.controller('CardCtrl', function($scope, TDCardDelegate) {
+  $scope.cardSwipedLeft = function(index) {
+    console.log('LEFT SWIPE');
+    $scope.addCard();
+  };
+  $scope.cardSwipedRight = function(index) {
+    console.log('RIGHT SWIPE');
+    $scope.addCard();
+  };
 });
