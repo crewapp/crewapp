445cee7278f8d8ee1be0eb987eb8890aa0ff27ed
Richard Kho <rkho@me.com>
2015-04-25 23:17:47 -0700

    Swiping is functional!

--- app2/www/js/swipe/swipe.js
+++ app2/www/js/swipe/swipe.js
@@ -1,10 +1,12 @@
 'use strict';
-angular.module('crewapp.swipe', [])
+angular.module('crewapp.swipe', ['ionic.contrib.ui.tinderCards'])
 .controller('SwipeController', function($scope, TDCardDelegate){
 
   $scope.test = 'hello';
 
   var cardTypes = [
+      // This should contain images of Crews to join!
+      // Additional prperties we could have are 'Room Name' so we can pass this on when someone swipes right
       { image: 'https://pbs.twimg.com/profile_images/546942133496995840/k7JAxvgq.jpeg' },
       { image: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png' },
       { image: 'https://pbs.twimg.com/profile_images/491995398135767040/ie2Z_V6e.jpeg' },
@@ -16,18 +18,18 @@ angular.module('crewapp.swipe', [])
   };
 
   $scope.addCard = function() {
+    // This is currently set to randomly add a card from the above list to the end of the list.
     var newCard = cardTypes[Math.floor(Math.random() * cardTypes.length)];
     newCard.id = Math.random();
     $scope.cards.push(angular.extend({}, newCard));
-  }
-})
-.controller('CardCtrl', function($scope, TDCardDelegate) {
+  };
   $scope.cardSwipedLeft = function(index) {
     console.log('LEFT SWIPE');
+    // Right now, swiping left will add a random card from the list to our list of available Crews. This could, in essence, duplicate rejected crews and add them to the end of the list.
     $scope.addCard();
   };
   $scope.cardSwipedRight = function(index) {
     console.log('RIGHT SWIPE');
-    $scope.addCard();
+    // Function here to send user to the appropriate chat room based on the card swiped to.
   };
-});
+})
