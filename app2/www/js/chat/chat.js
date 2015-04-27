18e27ea3e138a58d6018a9db76ad2692fc4b0c77
Pavan Ravipati <psravipati@gmail.com>
2015-04-26 20:03:37 -0700

    (fix) Add nonce to prevent ng-repeat duplication problem

--- app2/www/js/chat/chat.js
+++ app2/www/js/chat/chat.js
@@ -1,6 +1,26 @@
 'use strict';
 angular.module('crewapp.chat', [])
 .controller('ChatController', function($scope, Sockets, $localStorage, $ionicScrollDelegate){
+
+	var nonce = function(length) {
+      var last = null
+      var repeat = 0
+
+      if (typeof length == 'undefined') length = 15
+
+      return function() {
+        var now = Math.pow(10, 2) * +new Date()
+        if (now == last) {
+            repeat++
+        } else {
+            repeat = 0
+            last = now
+        }
+        var s = (now + repeat).toString()
+        return +s.substr(s.length - length)
+      }
+    }
+
 	$scope.form = {};
     Sockets.emit('join room', 'potatoes');
     $scope.name = $localStorage.name
