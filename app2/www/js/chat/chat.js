7f63a8749add30942229120eee66bb6a49373d3b
Arian Faurtosh <arian@icloud.com>
2015-04-27 11:49:06 -0700

    fixed bad styling

--- app2/www/js/chat/chat.js
+++ app2/www/js/chat/chat.js
@@ -3,25 +3,27 @@ angular.module('crewapp.chat', [])
 .controller('ChatController', function($scope, Sockets, $localStorage, $ionicScrollDelegate){
 
 	var nonce = function(length) {
-      var last = null
-      var repeat = 0
+    var last = null;
+    var repeat = 0;
 
-      if (typeof length == 'undefined') length = 15
-
-      return function() {
-        var now = Math.pow(10, 2) * +new Date()
-        if (now == last) {
-            repeat++
-        } else {
-            repeat = 0
-            last = now
-        }
-        var s = (now + repeat).toString()
-        return +s.substr(s.length - length)
-      }
+    if (typeof length === 'undefined'){
+      length = 15;
     }
 
-    var random = nonce(10);
+    return function() {
+      var now = Math.pow(10, 2) * +new Date()
+      if (now === last) {
+          repeat++;
+      } else {
+          repeat = 0;
+          last = now;
+      }
+      var s = (now + repeat).toString();
+      return +s.substr(s.length - length);
+    };
+  };
+
+  var random = nonce(10);
 
 	$scope.form = {};
     Sockets.emit('join room', 'potatoes');
@@ -30,14 +32,14 @@ angular.module('crewapp.chat', [])
     $scope.addItem = function() {
       var message = {name: $localStorage.name, message: $scope.form.itemToAdd, picture: $localStorage.picture}
 	    Sockets.emit('message', message);
-    }
-    
+    };
+
     Sockets.on('message', function(message) {
       var key = Date.now() + random();
 	    $scope.messages.push({key: key, message: message.message, name: message.name, picture: message.picture});
 	    $ionicScrollDelegate.scrollBottom();
-    })
-    
-    $scope.messages = []
+    });
+
+    $scope.messages = [];
 
 });
